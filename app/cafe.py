9c459ce039f72fe2dfd7824511d0a5542d1f799f
Viacheslav Kaliak <trinidaa@gmial.com>
2024-11-20 16:17:47 +0100

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -8,10 +8,7 @@ import datetime
 
 class Cafe:
 
-    def __init__(
-        self,
-        name: str,
-    ) -> None:
+    def __init__(self, name: str,) -> None:
         self.name = name
 
     def visit_cafe(
@@ -21,19 +18,14 @@ class Cafe:
         if visitors.get("vaccine") is None:
             raise NotVaccinatedError(
                 f"{visitors.get("name")} should "
-                f"vaccinate before visit cafe"
-            )
-        elif (
-            visitors["vaccine"].get("expiration_date") < datetime.date.today()
-            or None
-        ):
+                "vaccinate before visit cafe")
+        elif (visitors["vaccine"].get("expiration_date") <
+                datetime.date.today()):
             raise OutdatedVaccineError(
                 f"{visitors.get("name")} should renew "
-                f"vaccine before visit cafe"
-            )
+                "vaccine before visit cafe")
         elif visitors.get("wearing_a_mask") is False or None:
             raise NotWearingMaskError(
-                f"Visitor {visitors.get("name")} is not wearing a mask"
-            )
+                f"Visitor {visitors.get("name")} is not wearing a mask")
         else:
             return f"Welcome to {self.name}"

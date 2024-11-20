9a9fcbf75d74a1f61ea73f932b947d9e83fdb06c
trinidaa <71452328+trinidaa@users.noreply.github.com>
2024-11-20 15:59:52 +0100

    Update cafe.py

--- app/cafe.py
+++ app/cafe.py
@@ -19,21 +19,15 @@ class Cafe:
         visitors: dict,
     ) -> str:
         if visitors.get("vaccine") is None:
-            raise NotVaccinatedError(
-                f"{visitors.get("name")} should "
-                "vaccinate before visit cafe"
-            )
-        elif (
-            visitors["vaccine"].get("expiration_date") < datetime.date.today()
-            or None
-        ):
+            raise NotVaccinatedError(f"{visitors.get("name")} should "
+                    "vaccinate before visit cafe")
+        elif (visitors["vaccine"].get("expiration_date") < datetime.date.today() 
+              or None):
             raise OutdatedVaccineError(
                 f"{visitors.get("name")} should renew "
-                "vaccine before visit cafe"
-            )
+                "vaccine before visit cafe")
         elif visitors.get("wearing_a_mask") is False or None:
             raise NotWearingMaskError(
-                f"Visitor {visitors.get("name")} is not wearing a mask"
-            )
+                f"Visitor {visitors.get("name")} is not wearing a mask")
         else:
             return f"Welcome to {self.name}"

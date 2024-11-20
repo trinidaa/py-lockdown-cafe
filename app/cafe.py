d1eb9f3754890088566230cfd163064662217913
trinidaa <71452328+trinidaa@users.noreply.github.com>
2024-11-20 16:05:32 +0100

    Update cafe.py

--- app/cafe.py
+++ app/cafe.py
@@ -6,27 +6,21 @@ from app.errors import (
 import datetime
 
 
-class Cafe:
+class Cafe:  
 
-    def __init__(
-        self,
-        name: str,
-    ) -> None:
-        self.name = name
+    def __init__(self, name: str) -> None:  
+        self.name = name  
 
-    def visit_cafe(
-        self,
-        visitors: dict,
-    ) -> str:
-        if visitors.get("vaccine") is None:
-            raise NotVaccinatedError(f"{visitors.get("name")} should "
-                    "vaccinate before visit cafe")
-        elif visitors["vaccine"].get("expiration_date") < datetime.date.today():
-            raise OutdatedVaccineError(
-                f"{visitors.get("name")} should renew "
-                "vaccine before visit cafe")
-        elif visitors.get("wearing_a_mask") is False:
-            raise NotWearingMaskError(
-                f"Visitor {visitors.get("name")} is not wearing a mask")
-        else:
+    def visit_cafe(self, visitors: dict) -> str:  
+        if visitors.get("vaccine") is None:  
+            raise NotVaccinatedError(f"{visitors.get('name')} should "  
+                                     "vaccinate before visiting the cafe")  
+        elif visitors["vaccine"].get("expiration_date") < datetime.date.today():  
+            raise OutdatedVaccineError(  
+                f"{visitors.get('name')} should renew "  
+                "vaccine before visiting the cafe")  
+        elif visitors.get("wearing_a_mask") is False:  
+            raise NotWearingMaskError(  
+                f"Visitor {visitors.get('name')} is not wearing a mask")  
+        else:  
             return f"Welcome to {self.name}"
